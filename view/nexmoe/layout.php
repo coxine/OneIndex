d656825e86fe6eb6e4e227787e62e65772f14559
coxine <59347549+coxine@users.noreply.github.com>
2021-07-21 17:07:55 +0800

    将地址栏移到标题栏

--- view/nexmoe/layout.php
+++ view/nexmoe/layout.php
@@ -11,14 +11,17 @@
 </head>
 <body class="mdui-theme-primary-blue-grey mdui-theme-accent-blue" style="font-family:ceym">
 	<header class="nav">
-		<div class="navSize">
-			<a href="/"><img class="avatar" src="//q.qlogo.cn/g?b=qq&nk=3301696646&s=100"/></a>
+		<div class="navSize mdui-valign">
+			<a href="/"><h1><?php e(config('site_name'));?></h1></a>
+			<?php foreach((array)$navs as $n=>$l):?>
+			<i class="mdui-icon material-icons mdui-icon-dark" style="margin:0;">chevron_right</i>
+			<a href="<?php e($l);?>"><?php e($n);?></a>
+			<?php endforeach;?>
+			<!--<a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>-->
 			<div class="navRight">
 				<ul class="navul">
-					<li class="navli"><a href="//newtea.ml" target="_blank">新茶博客</a></li>
-					<li class="navli"><a href="//bbs.newtea.ml">新茶论坛</a></li>
-					<li class="navli"><a href="//costg.cf">余弦官网</a></li>
-					<li class="navli"><a href="//voice.costg.cf">余弦之声</a></li>
+					<li class="navli"><a href="//newtea.ml" target="_blank">新茶网<a></li>
+					<li class="navli"><a href="//costg.cf">余弦网</a></li>
 					<li class="navli"><a href="/admin">管理区</a></li>
 				</ul>
 				<div class="icon"></div>
@@ -26,16 +29,7 @@
 		</div>
 	</header>
 	<div class="mdui-container">
-	    <div class="mdui-container-fluid">
-	    <div class="mdui-toolbar nexmoe-item">
-			<a href="/"><?php e(config('site_name'));?></a>
-			<?php foreach((array)$navs as $n=>$l):?>
-			<i class="mdui-icon material-icons mdui-icon-dark" style="margin:0;">chevron_right</i>
-			<a href="<?php e($l);?>"><?php e($n);?></a>
-			<?php endforeach;?>
-			<!--<a href="javascript:;" class="mdui-btn mdui-btn-icon"><i class="mdui-icon material-icons">refresh</i></a>-->
-		</div>
-		</div>
+
     	<?php view::section('content');?>
   	</div>
 	<div class="disp-none">
